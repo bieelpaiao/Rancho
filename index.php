75ad726382a51e934d4115e816a41b763c5aa444
bieelpaiao <bibi.paiao@gmail.com>
2023-03-20 21:49:05 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,273 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Rancho Gardênia</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link href="assets/css/styles.css" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/18c0c84c7c.js" crossorigin="anonymous"></script>
+    </head>
+
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg fixed-top navbar-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src="./assets/img/logo_cia2.png" alt="Logo Cia da Pesca" id="navbar-logo">
+                    </a>
+
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Início</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#about-area">Sobre nós</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">A Casa</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#services-area">Comodidades</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#contact-title">Reserve</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#location-area">Localização</a>
+                            </li>                        
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+
+        <main>
+            <div id="mainSlider" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#mainSlider" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#mainSlider" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#mainSlider" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#mainSlider" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                    <button type="button" data-bs-target="#mainSlider" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                </div>    
+
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="./assets/img/pescaria.jpg" class="d-block w-100 carousel-img" alt="Pescaria">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h2>Pescaria</h2>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <img src="./assets/img/churrasco.jpg" class="d-block w-100 carousel-img" alt="Churrasco">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h2>Área de Churrasco</h2>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <img src="./assets/img/casinha.jpg" class="d-block w-100 carousel-img" alt="Casa na Árvore">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h2>Casa na Árvore</h2>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <img src="./assets/img/lazer.jpg" class="d-block w-100 carousel-img" alt="Lazer">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h2>Lazer</h2>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <img src="./assets/img/beira-rio.jpg" class="d-block w-100 carousel-img" alt="Beira do Rio">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h2>Beira do Rio</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <button class="carousel-control-prev" type="button" data-bs-target="#mainSlider" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+
+                <button class="carousel-control-next" type="button" data-bs-target="#mainSlider" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+
+            <div id="about-area">
+                <div class="container text-center">
+                    <div class="row align-items-center">
+                        <div class="col-12">
+                            <h3 class="main-title">Sobre nós</h3>
+                        </div>
+
+                        <div class="col-md-6">
+                            <img class="img-fluid" src="./assets/img/peixes.jpg" alt="Patrícia Gazeta">
+                        </div>
+
+                        <div class="col-md-6 about-text d-flex justify-content-center">
+                            <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="services-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <h3 class="main-title">Comodidades</h3>
+                        </div>
+
+                        <div class="col-md-4 service-box" class="card-link" data-toggle="modal" data-target="#modal1" >
+                            <i class="fa-solid fa-person-swimming"></i>
+                            <h4>Piscina Infantil</h4>
+                        </div>
+
+                        <div class="col-md-4 service-box"class="card-link" data-toggle="modal" data-target="#modal2" >
+                            <i class="fa-solid fa-sailboat"></i>
+                            <h4>Beira-rio</h4>
+                        </div>
+
+                        <div class="col-md-4 service-box"class="card-link" data-toggle="modal" data-target="#modal3" >
+                            <i class="fa-solid fa-toilet"></i>
+                            <h4>1 banheiro</h4>
+                        </div>
+
+                        <div class="col-md-4 service-box"class="card-link" data-toggle="modal" data-target="#modal4" >
+                            <i class="fa-solid fa-bed"></i>
+                            <h4>Quarto para 6 pessoas</h4>
+                        </div>
+
+                        <div class="col-md-4 service-box"class="card-link" data-toggle="modal" data-target="#modal4" >
+                            <i class="fa-solid fa-wind"></i>
+                            <h4>Ar-condicionado</h4>
+                        </div>
+
+                        <div class="col-md-4 service-box"class="card-link" data-toggle="modal" data-target="#modal4" >
+                            <i class="fa-solid fa-house"></i>
+                            <h4>Casa na árvore</h4>
+                        </div>    
+                    </div>
+                </div>
+            </div>
+
+            <div id="contact-title">
+                <div class="container-fluid text-center">
+                    <div class="row align-items-center">
+                        <div class="col-12" id="bg-contact-title">
+                            <h3 class="contact-title">Reserve Agora!</h3>
+                            <p class="contact-subtitle">Escolha uma data para reservar o local</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div id="contact-content">
+                <div class="container-fluid text-center">
+                    <div class="row align-items-center ps-5 pe-5">
+                        <div class="col-md-6 p-5">
+                            <a href="mailto:vanderclei05@gmail.com" target="blank">
+                                <div class="row align-items-center rounded-3 p-2 mt-3" id="contact-card">
+                                    <div class="col-md-2">
+                                        <i class="fa-regular fa-envelope"></i>
+                                    </div>
+
+                                    <div class="col-md-10">
+                                        <p id="card-text">Clique aqui e nos envie uma mensagem via e-mail!</p>
+                                    </div>
+                                </div>
+                            </a>
+
+                            <div class="text-center my-3" id="social-media">
+                                <p>Siga-nos em nossas redes sociais</p>
+                            </div>
+
+                            <div class="row align-items-center rounded-3 p-2 mt-3" id="contact-medias">
+                                <div class="col">
+                                    <a href="" target="blank"> <i class="fa-brands fa-facebook mx-3"></i> </a>
+                                    <a href="" target="blank"> <i class="fa-brands fa-instagram mx-3"></i> </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 m-0 px-5">
+                            <div class="contact-form-bg p-3">
+                                <form class="row g-3">
+                                    <div class="col-md-12 text-start">
+                                        <label for="inputName" class="form-label">Nome Completo</label>
+                                        <input type="text" class="form-control" id="inputName">
+                                    </div>
+
+                                    <div class="col-md-12 text-start">
+                                        <label for="inputEmail" class="form-label">E-mail</label>
+                                        <input type="text" class="form-control" id="inputEmail">
+                                    </div>
+
+                                    <div class="col-md-12 text-start">
+                                        <label for="inputMessage" class="form-label">Mensagem</label>
+                                        <textarea class="form-control" id="inputMessage"></textarea>
+                                    </div>
+
+                                    <div class="col-12">
+                                        <button type="submit" class="btn btn-primary">ENVIAR</button>
+                                    </div>
+                                </form>  
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="location-area">
+                <div class="container text-center">
+                    <div class="row align-items-center">
+                        <div class="col-12">
+                            <h3 class="main-title">Onde Estamos</h3>
+                        </div>
+
+                        <div class="col-12">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1841.443653091019!2d-50.904992088127905!3d-22.62068309110906!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbr!4v1679353317157!5m2!1sen!2sbr" width="1100" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <div class="circle-bg-wpp d-flex align-items-center justify-content-center">
+            <a class="wpp-btn" href="https://api.whatsapp.com/send/?phone=5518998124063&text=Ol%C3%A1!%20Vi%20seu%20Rancho%20no%20site." target="_blank">
+                <svg enable-background="new 0 0 512 512" width="50" height="50" version="1.1" viewBox="0 0 512 512" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><path d="M256.064,0h-0.128l0,0C114.784,0,0,114.816,0,256c0,56,18.048,107.904,48.736,150.048l-31.904,95.104  l98.4-31.456C155.712,496.512,204,512,256.064,512C397.216,512,512,397.152,512,256S397.216,0,256.064,0z" fill="#4CAF50"/><path d="m405.02 361.5c-6.176 17.44-30.688 31.904-50.24 36.128-13.376 2.848-30.848 5.12-89.664-19.264-75.232-31.168-123.68-107.62-127.46-112.58-3.616-4.96-30.4-40.48-30.4-77.216s18.656-54.624 26.176-62.304c6.176-6.304 16.384-9.184 26.176-9.184 3.168 0 6.016 0.16 8.576 0.288 7.52 0.32 11.296 0.768 16.256 12.64 6.176 14.88 21.216 51.616 23.008 55.392 1.824 3.776 3.648 8.896 1.088 13.856-2.4 5.12-4.512 7.392-8.288 11.744s-7.36 7.68-11.136 12.352c-3.456 4.064-7.36 8.416-3.008 15.936 4.352 7.36 19.392 31.904 41.536 51.616 28.576 25.44 51.744 33.568 60.032 37.024 6.176 2.56 13.536 1.952 18.048-2.848 5.728-6.176 12.8-16.416 20-26.496 5.12-7.232 11.584-8.128 18.368-5.568 6.912 2.4 43.488 20.48 51.008 24.224 7.52 3.776 12.48 5.568 14.304 8.736 1.792 3.168 1.792 18.048-4.384 35.52z" fill="#FAFAFA"/></svg>
+            </a>
+        </div>
+
+        <footer>
+            <div id="copy-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img id="logo-copy" src="./assets/img/logo_cia2.png" alt="LAMOAM">
+                            <p>Copyright <a href="">Rancho Cia da Pesca</a> &copy; 2023 </br> Desenvolvido por  <a href="https://www.linkedin.com/in/gabrielpaiao/" target="_blank">Gabriel Paião</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    </body>
+</html>
